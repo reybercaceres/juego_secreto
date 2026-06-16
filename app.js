b60637a39f25a0a1d7ff167053df92c4af0e802f
Reyber Jose Caceres Marquez <reybercaceres@gmail.com>
2026-06-16 13:58:04 -0400

    Ajuste del valor maximo

---
+++ app.js
@@ -0,0 +1,78 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 100;
+//console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;    
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+    }else{
+        // El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El numero secreto es menor');            
+        }else{
+            asignarTextoElemento('p','El numero secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    //let valorCaja = document.querySelector('#valorUsuario');
+   // valorCaja.value = '';
+
+   document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    // si ya sorteamos todos los numeros
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles')
+    }else{
+        // Si el numero generado esta incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        }else{
+            // almacenar el numero en la lista
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del numero secreto');
+    asignarTextoElemento('p',`Indica un numero del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //limpiar la caja
+    limpiarCaja();
+    // indicar mensaje de intervalo de numeros 
+    //generar el numero aleatorio
+    //inicializar el numero de intentos
+    condicionesIniciales();   
+    // desactivar el boton de nuevo juego
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+condicionesIniciales();
